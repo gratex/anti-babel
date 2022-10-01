9e0bb3f8d3a98aef5df8ea05fd43587e7b80a1ea
Marko Martin <marcus@gratex.com>
2022-10-01 21:55:57 +0200

    [improved dstring.trim now does not produce extra || []

--- samples/dojo/dstring.trim.js
+++ samples/dojo/dstring.trim.js
@@ -1,27 +1,20 @@
-
 require(["dojo/string"], function(dstring) {
-	
-	//console.log(dstring);
-	var x=" aaa ";
-	var xx=dstring.trim(x);
-	console.log("xx:"+xx);
-	console.assert(xx==='aaa');
+
+  var x = " aaa ";
+  var xx = dstring.trim(x);
+  console.log("xx:" + xx);
+  console.assert(xx === 'aaa');
 
 
-	var y=" aaa ";
-	var yy=dstring.trim(y || "");
-	console.log("yy:"+yy);
-	console.assert(yy==='aaa');
+  var y = " aaa ";
+  var yy = dstring.trim(y || "");
+  console.log("yy:" + yy);
+  console.assert(yy === 'aaa');
 
 
-	var z;
-	var zz=dstring.trim(z || "");
-	console.log("zz:"+zz);
-	console.assert(zz==='');
+  var z;
+  var zz = dstring.trim(z || "");
+  console.log("zz:" + zz);
+  console.assert(zz === '');
 
 });
-
-
-
-
-
